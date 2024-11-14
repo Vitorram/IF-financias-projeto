3f625f3f7db1a6e10b85db1f67c8067980ce0298
Vitorram <vitorramosmeneses11@gmail.com>
2024-11-14 20:43:55 -0300

    fofofo

--- js/main.js
+++ js/main.js
@@ -14,6 +14,11 @@ function closeModal (event){
     if (event.target.className==='modal')
         return modal.style.display = 'none'
    
-        if (event===undefined)
-        return cadastroUnico.style.display = 'none'
+       
+}
+function handleSubmitAddTiker(event){
+    //previni o comportamento padrao
+    event.preventDefault()
+    //pega o valor do bagulho (urlLOGO)
+    console.log(event.target.urlLogo.value)
 }
