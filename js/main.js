ce676fea07259e259087a2abe9d7ce441034a3ce
Vitorram <vitorramosmeneses11@gmail.com>
2024-11-14 19:25:18 -0300

    lego

--- js/main.js
+++ js/main.js
@@ -5,11 +5,15 @@ function openModal(){
 }
 function closeModal (event){
     const modal = document.querySelector ('.modal')
-    
+
+    const cadastroUnico = document.getElementById ("cadastroUnico")
+
     if (event ===undefined)
         return modal.style.display = 'none'
 
     if (event.target.className==='modal')
         return modal.style.display = 'none'
    
+        if (event===undefined)
+        return cadastroUnico.style.display = 'none'
 }
